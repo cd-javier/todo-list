65274791ad0ea6f072610e1449ad10edca1aca54
Javier Quiroga <contact@javierquiroga.com>
2025-01-01 16:47:13 +0000

    Update date behavior
    Add date-fns API.
    Create getter and setter methods for dueDate

--- src/index.js
+++ src/index.js
@@ -1,40 +1,51 @@
 import "./styles.css";
+import { format } from "date-fns";
 
-  const todoItems = [];
+const todoItems = [];
 
-  class ChecklistItem {
-    constructor(name) {
-      this.name = name;
-      this.completed = false;
-    }
-
-    toggleComplete() {
-        this.completed = !this.completed
-    }
+class ChecklistItem {
+  constructor(name) {
+    this.name = name;
+    this.completed = false;
   }
 
-  class TodoItem extends ChecklistItem {
-    constructor(name, description, dueDate, category, priority, notes) {
-      super(name);
-      this.description = description;
-      this.dueDate = dueDate;
-      this.category = category || "general";
-      this.priority = priority || 0;
-      this.notes = notes;
-      this.checklist = [];
-      this.completed = false;
-    }
+  toggleComplete() {
+    this.completed = !this.completed;
+  }
+}
 
-    editProperty(key, value) {
-        this[key] = value
-    }
-
-    addChecklistItem(value) {
-        this.checklist.push(new ChecklistItem(value))
-    }
+class TodoItem extends ChecklistItem {
+  constructor(name, description, category, priority, notes) {
+    super(name);
+    this.description = description;
+    this.category = category || "general";
+    this.priority = priority || 0;
+    this.notes = notes;
+    this.checklist = [];
+    this.completed = false;
   }
 
-  function createTodoItem(name, description, dueDate, category, priority, notes) {
-    const item = new TodoItem(name, description, dueDate, category, priority, notes);
-    todoItems.push(item)
+  set dueDate(date) {
+    this._dueDate = new Date(date);
   }
+
+  get dueDate() {
+    return format(this._dueDate, "dd MMM yyyy");
+  }
+
+  addChecklistItem(value) {
+    this.checklist.push(new ChecklistItem(value));
+  }
+}
+
+function createTodoItem(name, description, dueDate, category, priority, notes) {
+  const item = new TodoItem(
+    name,
+    description,
+    dueDate,
+    category,
+    priority,
+    notes
+  );
+  todoItems.push(item);
+}
